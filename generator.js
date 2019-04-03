6b59bd651261fe22c3a6ab54b1f0c25e80a760aa
Jaume Segarra <xumet12@icloud.com>
2019-04-03 22:12:15 +0200

    Solved bug with relative paths

--- generator.js
+++ generator.js
@@ -100,7 +100,7 @@ module.exports = (generatorName, stuffPath) => {
     if (fs.existsSync(generatorTemplateFolder)) {
 
         const stuffName = getStuffName(stuffPath);
-        if(stuffPath[0] !== '/') stuffPath = path.join(process.cwd(), stuffPath);
+        if(stuffPath[0] !== '/') stuffPath = path.join(process.env.INIT_CWD, stuffPath);
         
         createTempFolder().then(cacheFolderPath => {
             copyResourcesToTempFolder(stuffName, generatorTemplateFolder, cacheFolderPath).then(() => {
